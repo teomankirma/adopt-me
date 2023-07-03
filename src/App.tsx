7c619b133ddb4924abe4c64480ef879f87ad8d33
teomankirma <83593653+teomankirma@users.noreply.github.com>
2023-07-03 17:54:22 +0300

    Revert "feat: setup redux toolkit"
    This reverts commit da1894ae9527c2a4c5c6bf19bf6a828c46934f66.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Provider } from "react-redux";
-import store from "./store";
+import { useState } from "react";
+import AdoptedPetContext from "./AdoptedPetContext";
 import Details from "./Details";
 import SearchParams from "./SearchParams";
+import { Pet } from "./APIResponsesTypes";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -16,10 +17,11 @@ const queryClient = new QueryClient({
 });
 
 const App = () => {
+  const adoptedPet = useState(null as Pet | null);
   return (
     <div>
       <BrowserRouter>
-        <Provider store={store}>
+        <AdoptedPetContext.Provider value={adoptedPet}>
           <QueryClientProvider client={queryClient}>
             <header>
               <Link to="/">Adopt Me!</Link>
@@ -29,7 +31,7 @@ const App = () => {
               <Route path="/" element={<SearchParams />} />
             </Routes>
           </QueryClientProvider>
-        </Provider>
+        </AdoptedPetContext.Provider>
       </BrowserRouter>
     </div>
   );
